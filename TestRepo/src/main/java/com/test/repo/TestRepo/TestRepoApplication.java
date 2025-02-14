36ba4971df3ad184b18af33666f4ee97f975ab48
irfan <irfankhan457@gmail.com>
2025-02-14 14:00:42 +0530

    Update TestRepoApplication.java
    update from main

--- TestRepo/src/main/java/com/test/repo/TestRepo/TestRepoApplication.java
+++ TestRepo/src/main/java/com/test/repo/TestRepo/TestRepoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestRepoApplication {
-
+	//Hello From Main
 	public static void main(String[] args) {
 		SpringApplication.run(TestRepoApplication.class, args);
 	}
